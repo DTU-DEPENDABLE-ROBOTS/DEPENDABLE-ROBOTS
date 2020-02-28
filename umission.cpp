bab0dabd494fb58c905578bb2c0859d63000b4cc
kberci <k.berci95@gmail.com>
2020-02-28 16:34:06 +0100

    added turn

--- umission.cpp
+++ umission.cpp
@@ -613,7 +613,10 @@ bool UMission::mission1(int & state)
 
         snprintf(lines[line++], MAX_LEN,   "vel=0.2, log=5, acc=2, white=1: xl>16, dist=2.5");
         snprintf(lines[line++], MAX_LEN,   ":xl < 4,dist=0.2");
-        snprintf(lines[line++], MAX_LEN,   "vel=0,event=1:time=0.1");
+        snprintf(lines[line++], MAX_LEN,   "vel=0:time=0.1");
+        snprintf(lines[line++], MAX_LEN,   "tr=0,vel=0.2:turn=-90");
+        snprintf(lines[line++], MAX_LEN,   "edgel=0, white=1: dist=0.5");, 
+        snprintf(lines[line++], MAX_LEN,   "vel=0, event=1:time=0.1");
         sendAndActivateSnippet(lines, line);
     
         // debug
